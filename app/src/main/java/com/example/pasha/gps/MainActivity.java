a7b9a779d52389d8e5193848ec1b7d252ad69a17
sarwanpasha <sarwanpasha@gmail.com>
2015-05-13 21:42:24 +0500

    Location Tracker
    Use to locate position using latitude lognitude

---
+++ app/src/main/java/com/example/pasha/gps/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.pasha.gps;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    LocationManager lm;
+    LocationListener locationListener;
+
+    private class MyLocationListener implements LocationListener
+    {
+        @Override
+        public void onLocationChanged(Location loc) {
+            if (loc != null) {
+                Toast.makeText(getBaseContext(),
+                        "Location changed : Lat: " + loc.getLatitude() +
+                                " Lng: " + loc.getLongitude(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Toast.makeText(getBaseContext(),
+                    "Provider: " + provider + " disabled",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Toast.makeText(getBaseContext(),
+                    "Provider: " + provider + " enabled",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status,
+                                    Bundle extras) {
+            String statusString = "";
+            switch (status) {
+                case android.location.LocationProvider.AVAILABLE:
+                    statusString = "available";
+                case android.location.LocationProvider.OUT_OF_SERVICE:
+                    statusString = "out of service";
+                case
+                        android.location.LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    statusString = "temporarily unavailable";
+            }
+            Toast.makeText(getBaseContext(),
+                    provider + " " + statusString,
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //---use the LocationManager class to obtain locations data---
+        lm = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new MyLocationListener();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //---request for location updates using GPS---
+        lm.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                0,
+                locationListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //---remove the location listener---
+        lm.removeUpdates(locationListener);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
